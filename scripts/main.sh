6f90958a39ca59eca3e687654e1ed556cafd1170
mayan_sharma <41385368+mayan-sharma@users.noreply.github.com>
2021-10-24 00:14:46 +0530

    refactor: seperated push and deploy scripts

---
+++ scripts/main.sh
@@ -0,0 +1,8 @@
+if [ ! -f "appspec.yml" ]; then
+    echo $PWD
+    echo "AppSpec.yml not found in project root"
+    exit 1
+fi
+
+sh ./push.sh
+sh ./deploy.sh
---
+++ scripts/push.sh
@@ -0,0 +1,6 @@
+aws deploy push \
+    --application-name ${AWS_APP_NAME} \
+    --description "Revision for the ${AWS_APP_NAME}" \
+    --no-ignore-hidden-files \
+    --s3-location s3://${AWS_PUBLISH_BUCKET_NAME}/${AWS_FILENAME}.zip \
+    --source .
